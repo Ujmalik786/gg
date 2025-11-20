a29a2dad93d99b6c5b139278f54f41bd9eb824c5
Ujmalik786 <umairatta307@gmail.com>
2025-11-20 16:34:50 +0500

    Update redirect handler to include Cache-Control

--- api/redirect.js
+++ api/redirect.js
@@ -1,4 +1,7 @@
 export default function handler(req, res) {
-    res.setHeader('Location', 'https://yt.webbloggin.online/nevir/');
-    res.status(302).end();
+    res.writeHead(302, {
+        'Location': 'https://yt.webbloggin.online/nevir/',
+        'Cache-Control': 'no-cache'
+    });
+    res.end();
 }
